19cd7e8fff31f62e2adbaba49fcb0b036f083310
Dent24 <cljhs90607@gmail.com>
2020-08-02 12:31:50 +0800

    連接線上JSON

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ new Vue({
         datas: null,
     },
     mounted() {
-        
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET","https://datacenter.taichung.gov.tw/swagger/OpenData/72b2e32b-74e4-4000-b920-7457c54565be",true);
+        xhr.send();
+
+        xhr.onload = () => {
+            var x = JSON.parse(xhr.responseText);
+            this.datas = x;
+            console.log(x);
+        }
     },
 })
